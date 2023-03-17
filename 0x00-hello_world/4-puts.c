0d18c2443aef33bda7e6bf33803b7faa6463e28f
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-03-17 02:22:00 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-int main(){
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+/*
+ * prrint by puts
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
