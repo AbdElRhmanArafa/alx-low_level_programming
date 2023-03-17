705fbda599cbb8c40c3a5451607f41a9b17f1046
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-03-17 03:48:23 +0200

    101-quote.c fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
-
 #include <stdio.h>
-
+#include <unistd.h>
 
 /**
  * main - prints exactly "and that piece of art is useful"
@@ -9,12 +8,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
- {
-	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long unsigned int  c = 0;
-	while (c < sizeof(x)){
-	  putchar(x[c]);
-	  c++;
-	}
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
- }
+}
