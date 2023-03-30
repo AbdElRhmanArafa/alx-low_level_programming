abeead24005e6887580a7c63adfae2f82b547b9d
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-03-30 14:19:43 +0200

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,25 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int sum = 0, i, n = 0,n2=0;
-  while (s1[n] != '\0')
-    {
-        n++;
-    }
-     while (s2[n2] != '\0')
-    {
-        n2++;
-    }
-    if(n2 >n)
-    n=n2;
-
-    for (i = 0; i < n; i++)
-    {
-        sum = s1[i] - s2[i];
-        if (sum != 0)
-        {
-            return sum;
-        }
-    }
-    return (0);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
