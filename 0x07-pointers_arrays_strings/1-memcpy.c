3431ad45c5e2e9e3a1baa123a0335ab3b9ee5093
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-04-03 23:26:22 +0200

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
 /**
  * *_memcpy - copies memory area
  * @dest: destination memory area
- * @src: memory area to copy from
+ * @src: socure memory area
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
