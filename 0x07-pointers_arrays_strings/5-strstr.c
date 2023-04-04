b43109b3fbcd944bd5a49480c2ac1fc4db826838
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-04-04 02:37:54 +0200

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,18 +4,17 @@
 /**
  * *_strstr - locates a substring
  * @haystack: string to search in
- * @needle: substring to look for first occurrence 
- *
+ * @needle: substring to look for first occurrence
  * Return: pointer to the beginning of the located substring
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-    while (*haystack != '\0')
-    {
-        if (*haystack == needle[0])
-        return haystack;
-        haystack++;
-    }
-    return (0);
+while (*haystack != '\0')
+{
+if (*haystack == needle[0])
+return (haystack);
+haystack++;
+}
+return (0);
 }
