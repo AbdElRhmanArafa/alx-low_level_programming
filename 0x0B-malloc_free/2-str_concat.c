0370c044eff931557b5d9a8632a166d42e6a4edf
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-12 00:41:04 +0200

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	len1 = strlen(s1);
-    len2 = strlen(s2);
+	len2 = strlen(s2);
 
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
