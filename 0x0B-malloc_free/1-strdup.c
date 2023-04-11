d16d9a934e04248a020dcfc03d3d2b764883b775
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-12 00:19:53 +0200

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,13 @@
 char *_strdup(char *str)
 {
 	int i = 0;
+	int len = 0;
 	char *ptr;
 
+	len = strlen(str);
 	if (str == NULL)
 		return (0);
-	ptr = (char *) malloc(sizeof(str));
+	ptr = (char *) malloc(len * sizeof(*str));
 	if (ptr == NULL)
 		return (0);
 	while ((ptr[i] = str[i]) != '\0')
