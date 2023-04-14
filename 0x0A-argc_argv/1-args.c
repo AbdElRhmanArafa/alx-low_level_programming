80780114dff2c09f086316eb0200c5bd8ce89081
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-14 22:28:28 +0200

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  *@argv: array of argument
  *Return: 0
  */
-int main(int argc, char __attribute__((__unused__)) * argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-    printf("%d\n", argc - 1);
-    return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
