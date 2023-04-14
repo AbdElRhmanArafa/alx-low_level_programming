928646fe82feb4b35f367ea3e556a779763e7f65
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-14 22:20:02 +0200

    add 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints program name
+*@argc: number of argument
+*@argv: array of argument
+*Return: 0
+*/
+int main(int  argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
