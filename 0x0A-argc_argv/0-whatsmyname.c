4c4ae44a64ca0059007386bfaff3308ea5f5660d
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-14 22:16:03 +0200

    fixsd

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - prints program name
- *@argc: number of arguments
- *@argv: array of arguments
- *Return: 0
- */
-int main(int argc, char *argv[])
+* main - prints program name
+*@argc: number of argument
+*@argv: array of argument
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
-	(void)argc;
+
 	return (0);
 }
