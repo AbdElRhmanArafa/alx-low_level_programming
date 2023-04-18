ce0fa78a8118119ab3343dd827e1f68d732575f2
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-18 05:04:23 +0200

    add without stander lib

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,37 @@
 #include "dog.h"
-#include <string.h>
+/**
+ *_strlen - returns length of
+ *a string
+ *@str: string to be counted
+ *Return: returns length of string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+	while (str)
+		len++;
+
+	return (len);
+}
+
+/**
+ *_strcopy - copy string pointed by src
+ *into dest variable
+ *@dest:buffer storing string copy
+ *@src: buffer storing string to copy
+ *Return:returns copied string
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+	return (dest);
+}
+
 /**
  *new_dog - creates a new dog
  *@name: name of new dog
@@ -19,14 +51,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (doggo == NULL)
 		return (NULL);
 
-	doggo->name = malloc(sizeof(char) * (strlen(name) + 1));
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (doggo->name == NULL)
 	{
 		free(doggo);
 		return (NULL);
 	}
 
-	doggo->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggo->owner == NULL)
 	{
 		free(doggo->name);
@@ -34,9 +66,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	doggo->name = strcopy(doggo->name, name);
+	doggo->name = _strcopy(doggo->name, name);
 	doggo->age = age;
-	doggo->owner = strcopy(doggo->owner, owner);
+	doggo->owner = _strcopy(doggo->owner, owner);
 
 	return (doggo);
 }
