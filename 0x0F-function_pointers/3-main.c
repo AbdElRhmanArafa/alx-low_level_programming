2281536a6adebb29714bd51c0eac826143875099
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-19 12:22:45 +0200

    try

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - choose (op)
- * @argc: number of argc
- * @argv:  argument value
+ * main - ...
+ * @argc: ...
+ * @argv: ...
  *
  * Return: ...
  */
 int main(int argc, char *argv[])
 {
-	int (*op_all)(int, int);
-	int a, b;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
-		return (98);
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	op_all = get_op_func(argv[2]);
-	if (op_all)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", op_all(a, b));
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
