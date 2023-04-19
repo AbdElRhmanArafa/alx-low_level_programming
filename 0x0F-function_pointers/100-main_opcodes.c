ec692702bd233e8829400a2d6369ebd0ce99d6ef
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-19 12:36:59 +0200

    last

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * main - check the code
+ * @argc: ....
+ * @argv: ....
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *ptrFun = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Erro\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hh%s", *ptrFun++, b ? " " : "\n");
+	return (0);
+}
