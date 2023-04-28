07c3324916920f206ec7e51d2178420be55dbb06
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-04-29 00:38:29 +0200

    fix 0 to 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
+
 /**
- * print_listint -  function that prints all the elements of a listint_t list.
- * @h: linked list of type listint_t to print
- * Return: Return: the number of nodes
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	listint_t *template_node = h;
+	const listint_t *template_node = h;
 	unsigned int count = 0;
 
-	if (!h)
-		return (NULL);
-	while (template_node)
+	while (template_node != NULL)
 	{
 		printf("%d\n", template_node->n);
 		template_node = template_node->next;
 		count++;
 	}
+
 	return (count);
 }
