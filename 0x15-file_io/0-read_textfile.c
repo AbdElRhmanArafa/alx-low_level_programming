3228e7e4bdc32dcddd503b31613a6bdd3b37ea08
AbdElrhman <abdelrhman.arafa660@gmail.com>
2023-05-09 02:40:49 +0200

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,17 +19,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 	readBuffer = read(fd, buffer, letters);
+	if (readBuffer < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[readBuffer] = '\0';
+	close(fd);
 	writeBuffer = write(STDOUT_FILENO, buffer, readBuffer);
 
-	if (readBuffer != -1 || writeBuffer != -1 || readBuffer != writeBuffer)
+	if (writeBuffer < 0)
 	{
 		free(buffer);
 		return (0);
 	}
 	free(buffer);
-	close(fd);
 
 	return (writeBuffer);
 }
