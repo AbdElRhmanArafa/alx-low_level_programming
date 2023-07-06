05a324a49aa3699f8b18b94c39c88f2c25896980
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-07-06 23:06:06 +0300

    hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,15 +7,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 1;
-
-	if (index > 64)
+	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
-
-	for (; index > 0; index--)
-		i *= 2;
-	if ((*n >> index) & 1)
-		*n -= i;
-
+	}
+	*n &= ~(1UL << index);
 	return (1);
 }
