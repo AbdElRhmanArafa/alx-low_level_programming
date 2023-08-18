c9127a7286ce6fba56f228eef6b162d3714e741f
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-18 03:06:00 +0300

    dlistint_len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - length of  list
+ * @h: header of list
+ * Return: length of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    size_t len = 0;
+    while (current != NULL)
+    {
+        len++;
+        current = current->next;
+    }
+    return (len);
+}
