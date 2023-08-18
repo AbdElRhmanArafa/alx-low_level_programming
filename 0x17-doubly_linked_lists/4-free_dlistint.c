36dfdd8e3d188fe50fa6ac69461125ea00ce9d1d
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-18 16:40:39 +0300

    free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head: list to remove
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
