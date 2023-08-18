0a4d4c1423d22f5df5005e8be4dedbebe25792d3
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-18 17:41:16 +0300

    delete_dnodeint_at_index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Deletes the node at a specific index
+ * @head: Pointer to a pointer to the head of the list
+ * @index: Index of the node to be deleted (starting from 0)
+ * Return: 1 if successful, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	if (*head == NULL)
+		return (-1);
+	dlistint_t *temp = *head;
+	unsigned int len = 0;
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp != NULL)
+	{
+		if (len == index)
+		{
+			temp->prev->next = temp->next;
+			temp->next->prev = temp->prev;
+			free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		len++;
+	}
+	return (-1);
+}
